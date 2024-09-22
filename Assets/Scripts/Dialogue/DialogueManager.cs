adf91d7f99871d5d82367f343e18df97703f9b5d
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-09-22 17:22:40 +0200

    dialogue starts when pressing t

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    public GameObject DialogueObject;
     private Queue<string> lines;
     // Start is called before the first frame update
     void Start()
     {
         lines = new Queue<string>();
+        HideDialogue();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartDialogue(new Dialogue{npcName = "hans"});
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.npcName);
+        ShowDialogue();
+
+    }
+
+    public void HideDialogue()
+    {
+        DialogueObject.SetActive(false);
+    }
+
+    public void ShowDialogue()
+    {
+        DialogueObject.SetActive(true);
     }
 }
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -10,5 +10,6 @@ public class DialogueTrigger : MonoBehaviour
     public void TriggerDialogue()
     {
         //FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        Debug.Log("interaction started");
     }
 }
