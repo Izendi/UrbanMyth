aef764795613ecabe12832ae00b05d6486489af0
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-09-22 16:05:49 +0200

    inital setup for dialogue manager script

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    private Queue<string> lines;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        lines = new Queue<string>();
     }
 }
