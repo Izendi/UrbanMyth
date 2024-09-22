52698cba5c8b19a7b6c7bb50c91116be7d314d39
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-09-22 17:37:46 +0200

    Create models for dialogue vuilding blocks

---
+++ Assets/Scripts/DialogueSystem/Models/DialogueNode.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.DialogueSystem.Models
+{
+    [System.Serializable]
+    public class DialogueNode
+    {
+        public int DialogueId { get; set; }
+        public string npcName{ get; set; }
+        public string Text { get; set; }
+        public Response[] Responses { get; set; }
+
+    }
+}
---
+++ Assets/Scripts/DialogueSystem/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.DialogueSystem.Models
+{
+    [System.Serializable]
+    public class Response
+    {
+        public string Text { get; set; }
+        public int NextDialogueId { get; set; }
+    }
+}
