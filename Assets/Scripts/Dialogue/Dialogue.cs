31db349012f979cb943602061f5460d1f52f0567
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-09-22 16:10:26 +0200

    Make the dialogue class into dialogue structure
    basically, we want to be able to make it possible to use this class as a parameter when we interact with objects and npc. So this class will be responsible for storing information  about the current interaction

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,39 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-
-public class Dialogue : MonoBehaviour
+[System.Serializable]
+public class Dialogue
 {
-    public TextMeshProUGUI textComponent;
+    public string npcName;
     public string[] lines;
-    public float printingSpeed;
-    public int index;
-    void Start()
-    {
-
-        textComponent.text = string.Empty;
-        StartDialogue();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    void StartDialogue()
-    {
-        index = 0;
-        StartCoroutine(PrintText());
-
-    }
-
-    IEnumerator PrintText()
-    {
-        foreach (char c in lines[index].ToCharArray())
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(printingSpeed);
-        }
-    }
 }
