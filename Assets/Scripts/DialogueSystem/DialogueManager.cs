9488033600de8784801d4e7f09dc1732e9085302
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-09-22 19:25:27 +0200

    Printing dialogue from file is working

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.DialogueSystem.Models;
+using TMPro;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
     public GameObject DialogueObject;
+    public TextMeshProUGUI DialogueText;
+    public TextAsset DialogueFile;
+    public float PrintSpeed;
+    public Dialogue CurrentDialogue;
+    private DialogueNode currentDialogueNode;
     private Queue<string> lines;
     // Start is called before the first frame update
     void Start()
     {
         lines = new Queue<string>();
+        CurrentDialogue = JsonUtility.FromJson<Dialogue>(DialogueFile.text);
         HideDialogue();
     }
 
@@ -17,15 +26,19 @@ public class DialogueManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            StartDialogue(new Dialogue{npcName = "hans"});
+            StartDialogue(1);
         }
     }
 
-    public void StartDialogue(Dialogue dialogue)
+    public void StartDialogue(int nodeId)
     {
-        Debug.Log("Starting conversation with " + dialogue.npcName);
-        ShowDialogue();
 
+        Debug.Log(CurrentDialogue.DialogueNodes.Find(x => x.DialogueId == nodeId));
+
+        currentDialogueNode = CurrentDialogue.DialogueNodes.Find(x => x.DialogueId == nodeId);
+        Debug.Log(currentDialogueNode.DialogueId);
+        Debug.Log(currentDialogueNode.Text);
+        ShowDialogue();
     }
 
     public void HideDialogue()
@@ -36,5 +49,26 @@ public class DialogueManager : MonoBehaviour
     public void ShowDialogue()
     {
         DialogueObject.SetActive(true);
+
+        StartCoroutine(TypeNpcLine());
+        //if (currentDialogueNode.Responses.Count > 0)
+        //{
+        //    for (int i = 0; i < currentDialogueNode.Responses.Count; i++)
+        //    {
+        //        Debug.Log((i + 1) + ": " + currentDialogueNode.Responses[i].Text);
+        //    }
+        //}
+    }
+
+    private IEnumerator TypeNpcLine()
+    {
+        DialogueText.text = "";
+
+        Debug.Log(currentDialogueNode.Text);
+        foreach (char c in currentDialogueNode.Text)
+        {
+            DialogueText.text += c;
+            yield return new WaitForSeconds(PrintSpeed);
+        }
     }
 }
