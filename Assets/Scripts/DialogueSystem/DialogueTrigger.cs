510825969885b4ecf704f1d32b7a49f330be986f
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-09-23 18:11:35 +0200

    create interact component with dialogue trigger

--- Assets/Scripts/DialogueSystem/DialogueTrigger.cs
+++ Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.DialogueSystem.Models;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
 
-    public Dialogue dialogue;
+    public TextAsset DialogueFile;
+
+    private bool isPlayerInRange = true;
+
+    public void Update()
+    {
+        if ( isPlayerInRange && Input.GetKeyDown(KeyCode.T))
+        {
+            TriggerDialogue();
+        }
+    }
 
     public void TriggerDialogue()
     {
