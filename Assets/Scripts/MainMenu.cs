2b35d77b4e2c69c48acc86c7f2443b4cb2aa5314
Izendi <cjwinters1404@gmail.com>
2024-10-01 12:32:41 +0200

    minor changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {   
     public void StartGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(2);
     }
 
     public void QuitGame()
