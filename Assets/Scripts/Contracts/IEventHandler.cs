5f62b68a238116d6b3bae9d494ed9e3a84e168f0
ingridstake <59291877+ingridstake@users.noreply.github.com>
2024-10-06 20:59:16 +0200

    Add classes and structures for eventaggregator

---
+++ Assets/Scripts/Contracts/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Assets.Scripts.Events;
+
+namespace Assets.Scripts.Contracts
+{
+    public interface IEventHandler<TEvent> where TEvent : IEvent
+    {
+        void Handle(TEvent @event);
+    }
+}
