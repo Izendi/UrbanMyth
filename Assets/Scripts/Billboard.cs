803e678445a793d3344be4a2b968971381c814f9
ingridstake <ingrid.stake@tickster.com>
2024-10-11 00:02:20 +0200

    add rotation based on player camera

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    [SerializeField]
+    private Camera playerCamera;
     // Update is called once per frame
     void Update()
     {
-        var yRotation = Camera.main.transform.rotation.eulerAngles.y;
+        Debug.Log(Camera.main);
+        //var yRotation = Camera.main.transform.rotation.eulerAngles.y;
+
+        var yRotation = playerCamera.transform.rotation.eulerAngles.y;
 
         transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
     }
