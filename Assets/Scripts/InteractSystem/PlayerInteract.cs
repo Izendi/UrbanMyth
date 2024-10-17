7b35bbb3e8315549e12396e972dc984e7e5452e7
ingridstake <ingridst@student.chalmers.se>
2024-10-17 10:51:17 +0200

    Clear transition logic from playerinteract

--- Assets/Scripts/InteractSystem/PlayerInteract.cs
+++ Assets/Scripts/InteractSystem/PlayerInteract.cs
@@ -4,7 +4,7 @@ using Assets.Scripts.Events;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerInteract : MonoBehaviour, IEventHandler<DialogueEndedEvent>
+public class PlayerInteract : MonoBehaviour
 {
     private const float INTERACT_DISTANCE = 2.0f;
     private const float OFFSET_DISTANCE = 1.5f;
@@ -18,48 +18,17 @@ public class PlayerInteract : MonoBehaviour, IEventHandler<DialogueEndedEvent>
     private Quaternion resetRotation;
     private Vector3 resetPosition;
 
-    void Start()
-    {
-        EventAggregator.Instance.Subscribe(this);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // If transitioning - that is, if the camera is moving to focus on the npc we are interacting with, update the camera's position and rotation smoothly
-        if (false) // (isTransitioning)
-        {
-            // Smoothly move the camera towards the target position
-            // PlayerCamera.transform.position = Vector3.Lerp(PlayerCamera.transform.position, targetPosition, Time.deltaTime * TRANSITION_SPEED);
-
-            // Smoothly rotate the camera towards the target rotation
-            PlayerCamera.transform.rotation = Quaternion.Lerp(PlayerCamera.transform.rotation, targetRotation, Time.deltaTime * TRANSITION_SPEED);
-
-            // Check if the camera has reached the target
-            if (Quaternion.Angle(PlayerCamera.transform.rotation, targetRotation) < 0.02f)
-                isTransitioning = false; // Stop transitioning
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, INTERACT_DISTANCE);
             foreach (var hitCollider in hitColliders)
             {
                 if (hitCollider.TryGetComponent(out InteractableNpc dialogueTrigger) && !DialogueManager.IsDialogueActive)
                 {
-                    FocusCamera(dialogueTrigger.transform);
                     dialogueTrigger.Interact();
                 }
-            }
-        }
-        else if (!DialogueManager.IsDialogueActive)
-        {
-            if (PlayerCamera.transform.position.x != transform.position.x ||
-                PlayerCamera.transform.position.z != transform.position.z)
-            {
-                var cameraHeight = PlayerCamera.transform.position.y;
-                var transitionTarget = new Vector3(transform.position.x, cameraHeight, transform.position.z);
-
-                PlayerCamera.transform.position = transitionTarget;
             }
         }
     }
@@ -74,28 +43,5 @@ public class PlayerInteract : MonoBehaviour, IEventHandler<DialogueEndedEvent>
         }
 
         return null;
-    }
-
-    private void FocusCamera(Transform target)
-    {
-        // Calculate the target rotation to look at the NPC
-        var direction = target.position - transform.position;
-        direction = new Vector3(-5.5f, PlayerCamera.transform.rotation.y, PlayerCamera.transform.rotation.z);
-
-        targetRotation = Quaternion.LookRotation(direction);
-
-        //Save the current camera position and rotation
-        resetPosition = PlayerCamera.transform.position;
-        resetRotation = PlayerCamera.transform.rotation;
-
-        // Start transitioning the camera
-        isTransitioning = true;
-    }
-
-    public void Handle(DialogueEndedEvent eventArgs)
-    {
-        isTransitioning = false;
-        PlayerCamera.transform.position = resetPosition;
-        PlayerCamera.transform.rotation = resetRotation;
     }
 }
