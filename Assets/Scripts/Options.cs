a94729125381a684d2bce195ab1c7e2c6361851f
hampusrs <hampusrhedinstam@gmail.com>
2024-10-21 02:41:48 +0200

    update menu

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -2,10 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetVolume (float volume)
     {
@@ -17,13 +50,17 @@ public class Options : MonoBehaviour
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void setFullscreen (bool isFullscreen)
     {
-        
+        Screen.fullScreen = isFullscreen;
     }
 
-    // Update is called once per frame
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     void Update()
     {
         
